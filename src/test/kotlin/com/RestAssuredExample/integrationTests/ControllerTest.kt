02aeaef6d86d1458869a00787a87b42184768773
Gabriel <gabriel@iMac.local>
2020-08-19 15:41:40 -0300

    test validando post

--- src/test/kotlin/com/RestAssuredExample/integrationTests/ControllerTest.kt
+++ src/test/kotlin/com/RestAssuredExample/integrationTests/ControllerTest.kt
@@ -14,13 +14,14 @@ import org.apache.http.entity.ContentType.APPLICATION_JSON
 import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [RestAssuredExampleApplication::class])
 class ControllerTest {
 
     @Autowired
     private lateinit var repository: guiaRepository
-    private val PATH: String = "/turismo"
+    private val ENDPOINT: String = "/turismo"
 
     @LocalServerPort
     internal var port: Int = 0
@@ -29,15 +30,10 @@ class ControllerTest {
     fun `Dado que a operação seja um sucesso é retornado uma lista com todos os guias turísticos`() {
         val findBD = repository.findAll()
         val findAPI = given()
-                .config(
-                    config()
-                        .encoderConfig(encoderConfig().encodeContentTypeAs("application/json", ContentType.TEXT))
-                )
                 .port(port)
                 .log().all()
-                .contentType(APPLICATION_JSON.toString())
                 .`when`()
-                .get("$PATH/guias")
+                .get("$ENDPOINT/guias")
                 .then()
                 .log().all()
                 .statusCode(OK.value())
@@ -48,6 +44,54 @@ class ControllerTest {
                 )
                 .extract().body().jsonPath().getList(".", turismo::class.java)
         assertThat(findAPI).isEqualTo(findBD)
+    }
+
+    @Test
+    fun `Quando um usuário realiza um POST com dados corretos a operação deve ser realizada com sucesso`() {
+        val guia = turismo(5, "Alberto Silva", "M", "Armação dos Búzios")
+        val payload = com.fasterxml.jackson.databind.ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(guia)
+
+        given()
+            .port(port)
+            .log().all()
+            .contentType(APPLICATION_JSON.toString())
+            .`when`()
+            .body(payload)
+            .post("$ENDPOINT")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.CREATED.value())
+
+
+        val id: Long = guia.id
+        val validatePlayer = repository.findById(id)
+
+        assertThat(validatePlayer.get()).usingRecursiveComparison().isEqualTo(guia)
+    }
+
+    @Test
+    fun `Quando realiza uma operação PATCH para alterar o nome com dados corretos a operação deve ser executada com sucesso`() {
+        val value = mapOf("nome" to "Shrek")
+        val id: Long = 1
+        val before = repository.findById(id)
+        val payload = com.fasterxml.jackson.databind.ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(value)
+
+        assertThat(before.get().nome).isNotEqualTo(payload)
+
+        given()
+            .port(port)
+            .log().all()
+            .contentType(APPLICATION_JSON.toString())
+            .`when`()
+            .body(payload)
+            .patch("$ENDPOINT/nome/$id")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.NO_CONTENT.value())
+
+        val after = repository.findById(id)
+        assertThat(after.get().nome).isEqualTo(value["nome"])
+        assertThat(after.get()).usingRecursiveComparison().ignoringFields("nome").isEqualTo(before.get())
     }
 
 }
